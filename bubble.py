73c4285e9494ed003b8f30ac2add0859bcabfdee
Anshu Rajpoot <107042248+Anshu-Rajpoot@users.noreply.github.com>
2023-02-15 12:17:22 +0530

    Add files via upload

---
+++ bubble.py
@@ -0,0 +1,14 @@
+# Bubble Sort
+def bs(a):
+    for i in range(0,len(a)):
+        for j in range(0,len(a)-1):
+            if a[j]>a[j+1]:
+                a[j],a[j+1]=a[j+1],a[j]
+data=[]
+n=int(input("How many Inputs are there : "))
+for i in range(n):
+    x=float(input("enter the Input number " +str(i+1)+" : "))
+    data.append(x)
+bs(data)
+print(data,sep="\n")
+
